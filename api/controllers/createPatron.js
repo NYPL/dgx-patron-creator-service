45dd5752711876d24fb6c0aec475501a442a54d2
Kang Peng <kangtingpeng@nypl.org>
2017-04-13 13:01:53 -0400

    Linted createPatron.js

--- api/controllers/createPatron.js
+++ api/controllers/createPatron.js
@@ -1,7 +1,20 @@
-'use strict';
+const axios = require('axios');
+const ccConfig = require('./../../config/ccConfig.js');
 
-var axios = require('axios');
-var ccConfig = require('./../../config/ccConfig.js');
+/**
+ * renderResponse(req, res, message)
+ * Render the response from Card Creator API.
+ *
+ * @param {req} HTTP request
+ * @param {res} HTTP response
+ * @param {message} object
+ */
+function renderResponse(req, res, message) {
+  res
+    .status(200)
+    .header('Content-Type', 'application/json')
+    .json(message);
+}
 
 /**
  * createPatron(req, res)
@@ -18,11 +31,11 @@ function createPatron(req, res) {
       .status(400)
       .header('Content-Type', 'application/json')
       .json({
-        "status_code": 400,
-        "type": "error_type",
-        "message": "No username",
-        "error": {},
-        "debug_info": {}
+        status_code: 400,
+        type: 'error_type',
+        message: 'No username',
+        error: {},
+        debug_info: {},
       });
 
     return;
@@ -42,33 +55,18 @@ function createPatron(req, res) {
       renderResponse(req, res, response.data);
     })
     .catch(response => {
-      let responseMessage = {
-        "status_code": response.status,
-        "type": "error_type",
-        "message": response.message,
-        "error": {},
-        "debug_info": {}
-      }
+      const responseMessage = {
+        status_code: response.status,
+        type: 'error_type',
+        message: response.message,
+        error: {},
+        debug_info: {},
+      };
+
       renderResponse(req, res, responseMessage);
     });
 }
 
-/**
- * renderResponse(req, res, message)
- * Render the response from Card Creator API.
- *
- * @param {req} HTTP request
- * @param {res} HTTP response
- * @param {message} object
- */
-function renderResponse(req, res, message) {
-  res
-    .status(200)
-    .header('Content-Type', 'application/json')
-    .json(message);
-}
-
 module.exports = {
-  createPatron: createPatron,
+  createPatron,
 };
-
