8d7315f0575b1142dd1bfc38edc4baeb65b8d639
Kang Peng <kangtingpeng@nypl.org>
2017-04-18 16:24:49 -0400

    Set up functions to check the error response data.

--- api/model/modelResponse.js
+++ api/model/modelResponse.js
@@ -1,3 +1,5 @@
+const url = require("url");
+
 function modelPatronCreatorResponse(data, status) {
   const detail = (data && data.debug_info) ? JSON.parse(data.debug_info) : {};
 
@@ -17,18 +19,32 @@ function modelPatronCreatorResponse(data, status) {
   };
 }
 
+function parseJSON(str) {
+  try {
+    return JSON.parse(str);
+  } catch (e) {
+    return str;
+  }
+}
+
 function modelErrorResponse(data) {
+  var typeSlug = '';
+
+  if (data && data.type) {
+    const typeURL = url.parse(data.type);
+    typeSlug = typeURL.pathname.split("/").pop();
+  }
 
   return {
     data: {
       status_code_from_card_creator: data.status || null,
-      type: data.type || null,
+      type: typeSlug,
       patron: null,
       simplePatron: null,
       message: data.message,
       detail: {
         title: data.title || '',
-        debug: (data.debug_message) ? JSON.parse(data.debug_message) : {},
+        debug: (data.debug_message) ? parseJSON(data.debug_message) : {},
       },
       count: 0,
     },
