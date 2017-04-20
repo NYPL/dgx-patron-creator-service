0350a127353c6eee3785c9823a1ce6c76ae2850a
Kang Peng <kangtingpeng@nypl.org>
2017-04-20 18:05:02 -0400

    Linted modelRequestBody.js

--- api/model/modelRequestBody.js
+++ api/model/modelRequestBody.js
@@ -1,23 +1,48 @@
-function extractSimplePatron(data) {
-  if(data && data.simplePatron) {
-    return data.simplePatron;
+/**
+ * extractSimplePatron(obj)
+ * Gets the item "simplePatron" out of the original request body from the client.
+ *
+ * @param {obj} object
+ * @return object
+ */
+function extractSimplePatron(obj) {
+  if (obj && obj.simplePatron) {
+    return obj.simplePatron;
   }
 
   return {};
   // here should return error response
 }
 
-function updateDateOfBirthToBirthdate(object) {
-  if (object && object.dateOfBirth) {
-    object.birthdate = object.dateOfBirth;
+/**
+ * updateDateOfBirthToBirthdate(obj)
+ * Replaces the item "dateOfBirth" by creating a new item "birthdate" and deleting "dateOfBirth".
+ *
+ * @param {obj} object
+ * @return object
+ */
+function updateDateOfBirthToBirthdate(obj) {
+  // Assignes a new object here to prevent the airbnb eslint rule:
+  // Reassignment of Function Parameters (no-param-reassign)
+  const newObj = obj;
+
+  if (newObj && newObj.dateOfBirth) {
+    newObj.birthdate = newObj.dateOfBirth;
   }
 
-  delete object.dateOfBirth;
-  return object;
+  delete newObj.dateOfBirth;
+  return newObj;
 }
 
-function modelSimplePatron(data) {
-  return updateDateOfBirthToBirthdate(extractSimplePatron(data));
+/**
+ * modelSimplePatron(obj)
+ * Takes the original request.body from the client and models it for the Card Creartor.
+ *
+ * @param {obj} object
+ * @return object
+ */
+function modelSimplePatron(obj) {
+  return updateDateOfBirthToBirthdate(extractSimplePatron(obj));
 }
 
 module.exports = {
