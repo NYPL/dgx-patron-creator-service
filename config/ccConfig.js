c45fdf4adc3a4643c9dea6066ed90c340285f8d3
Lucas Cioffi <lucas.cioffi@gmail.com>
2018-06-22 15:49:36 -0400

    Decrypting AWS environment variables

--- config/ccConfig.js
+++ config/ccConfig.js
@@ -1,5 +1,31 @@
 // The credentials for NYPL's Simplified Card Creator API
+require('dotenv').config();
+
+const aws = require('aws-sdk');
+let decryptKMS;
+let kms;
+
+kms = new aws.KMS({
+  region: 'us-east-1',
+});
+
+decryptKMS = (key) => {
+  const params = {
+    CiphertextBlob: new Buffer(key, 'base64'),
+  };
+
+  return new Promise((resolve, reject) => {
+    kms.decrypt(params, (err, data) => {
+      if (err) {
+        reject(err);
+      } else {
+        resolve(data.Plaintext.toString());
+      }
+    });
+  });
+};
+
 module.exports = {
-  username: process.env.CARD_CREATOR_USERNAME,
-  password: process.env.CARD_CREATOR_PASSWORD
+  username: decryptKMS(process.env.CARD_CREATOR_USERNAME),
+  password: decryptKMS(process.env.CARD_CREATOR_PASSWORD)
 };
